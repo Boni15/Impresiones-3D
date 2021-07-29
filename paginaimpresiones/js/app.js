1af703a1fea06d5d50c5a4d956211834f4496dea
Boni15 <67567813+Boni15@users.noreply.github.com>
2021-07-29 18:12:52 -0300

    Add files via upload

---
+++ paginaimpresiones/js/app.js
@@ -0,0 +1,7 @@
+let nombre =prompt("ingrese su nombre") 
+let calle = prompt("ingrese su calle:")
+let num = prompt("Ingrese el numero de su casa")
+let mensaje = "Su nombre es" + nombre
+console.log(mensaje)
+alert(mensaje)
+num = parseInt(num)
